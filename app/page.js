3e0a10066e364f73503b53db4f80f1cdc4905ebe
LordOnionRings <michael.arone@gmail.com>
2025-12-02 14:06:50 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Rubric Creator</h1>
+      <p>Your app is successfully deployed.</p>
+    </main>
+  );
+}
